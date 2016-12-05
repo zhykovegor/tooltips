4f83a85d4dcda29955758ec8520cba4c64cc2173
zhykovegor <zhykovegor@gmail.com>
2016-12-05 08:42:35 -0800

    Fix bags with locale storage

--- src/main.js
+++ src/main.js
@@ -1,1 +1,46 @@
-console.log("it's good!");
+/***
+ * Main functionality
+ *
+ */
+
+var data;
+
+$(document).ready(function() {
+  //init masonry
+  $(".grid").masonry({
+    itemSelector: ".grid-item",
+    columnWidth: 200
+  });
+
+  //event handler for button add
+});
+
+//saving settings to local store
+window.onunload = function() {
+    localStorage.setItem("data", JSON.stringify(data));
+};
+
+//loading settings from local storage
+window.onload = function() {
+    data = JSON.parse(localStorage.getItem("data"));
+    if(data == null) {
+      data = [];
+    }
+};
+
+//angular setting
+var toolTipsApp = angular.module('tooltips', []);
+
+//create new tip
+function Tip(value) {
+  return {text: value};
+};
+
+toolTipsApp.controller('contentCtrl', function ($scope) {
+  $scope.tips = data;
+
+  $scope.update = function() {
+    alert($("#text").val());
+    data.push(new Tip($("#text").val()));
+  };
+});
--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,6 @@ module.exports = {
         path: __dirname + '/public/build/',
         publicPath: "build/",
         filename: "bundle.min.js"
-    },
-    plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      include: /\.min\.js$/,
-      minimize: true
-    })
-  ]
+    }
+  
 }
